9a8a10ef5e891475263f896f6624f45b80e9a1e8
giuseppe0908 <placidagiuseppe@gmail.com>
2023-06-23 10:39:45 +0200

    create backend

---
+++ back-end/dot/src/main/java/com/biblioteca/dot/DotApplication.java
@@ -0,0 +1,13 @@
+package com.biblioteca.dot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotApplication.class, args);
+	}
+
+}
